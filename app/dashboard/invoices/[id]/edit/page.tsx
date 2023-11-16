e38a2898d0fbefde32a1ed3eb9b0770448708eae
Romel Rey Silva <sromelrey@gmail.com>
2023-11-16 10:14:39 +0800

    fix type error build

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -6,8 +6,10 @@ import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
-  const [invoice, customers]: [InvoiceForm, CustomerField[]] =
-    await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
 
   if (!invoice) {
     notFound();
